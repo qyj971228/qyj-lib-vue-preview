7cba6f5ed6c1f24de08609ca7fd6c0828d1e9148
qyj <1078869122@qq.com>
2024-02-11 01:01:53 +0800

    feat: preview

---
+++ src/demo.vue
@@ -0,0 +1,82 @@
+<script setup lang="ts">
+import { Button, Dropdown, Menu } from 'qyj-lib-vue/dist/lib'
+
+function onSelect(item: any, index: any) {
+  console.log(item, index)
+}
+</script>
+
+<template>
+  <div>
+    <h3>Dropdown</h3>
+    <div style="padding-top: 100px; padding-left: 150px">
+      <Dropdown
+        position="top-left"
+        :data="[{ name: 'item1' }, { name: 'item2' }]"
+        @onSelect="onSelect"
+      >
+        <Button size="l">top-left</Button>
+      </Dropdown>
+      <Dropdown position="top" :data="[{ name: 'item1' }, { name: 'item2' }]">
+        <Button>top</Button>
+      </Dropdown>
+      <Dropdown
+        position="top-right"
+        :data="[{ name: 'item1' }, { name: 'item2' }]"
+      >
+        <Button size="s">top-right</Button>
+      </Dropdown>
+    </div>
+    <div style="padding-top: 100px; padding-left: 150px">
+      <Dropdown position="left" :data="[{ name: 'item1' }, { name: 'item2' }]">
+        <Button>left</Button>
+      </Dropdown>
+      <Dropdown position="right" :data="[{ name: 'item1' }, { name: 'item2' }]">
+        <Button>right</Button>
+      </Dropdown>
+    </div>
+    <div style="padding-top: 100px; padding-left: 150px">
+      <Dropdown
+        position="bottom-left"
+        :data="[{ name: 'item1' }, { name: 'item2' }]"
+      >
+        <Button weight="primary">bottom-left</Button>
+      </Dropdown>
+      <Dropdown
+        position="bottom"
+        :data="[{ name: 'item1' }, { name: 'item2' }]"
+      >
+        <Button weight="primary" kind="warn"> bottom </Button>
+      </Dropdown>
+      <Dropdown
+        position="bottom-right"
+        :data="[{ name: 'item1' }, { name: 'item2' }]"
+      >
+        <Button weight="primary" kind="error"> bottom-right </Button>
+      </Dropdown>
+    </div>
+    <h3>Menu</h3>
+    <Menu
+      :onSelect="onSelect"
+      :data="[
+        { name: '1' },
+        { name: '2' },
+        {
+          name: '3',
+          children: [
+            {
+              name: '3-1',
+              children: [
+                { name: '3-1-1', children: [{ name: '3-1-1-1' }] },
+                { name: '3-1-2', children: [{ name: '3-1-2-2' }] },
+              ],
+            },
+            { name: '3-2' },
+          ],
+        },
+        { name: '4' },
+        { name: '5' },
+      ]"
+    ></Menu>
+  </div>
+</template>
